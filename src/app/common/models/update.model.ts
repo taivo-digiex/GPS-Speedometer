35f1f113cc15c56304c7543c61d6550c602b2c67
vdt2210 <vdt1999@outlook.com>
2022-03-26 09:48:44 +0700

    update style and update model

--- src/app/common/models/update.model.ts
+++ src/app/common/models/update.model.ts
@@ -4,26 +4,7 @@ export interface AppUpdateModel {
   upload_url: string;
   html_url: string;
   id: number;
-  author: {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-  };
+  author: author;
   node_id: string;
   tag_name: string;
   target_commitish: string;
@@ -32,43 +13,66 @@ export interface AppUpdateModel {
   prerelease: boolean;
   created_at: string;
   published_at: string;
-  assets: [
-    {
-      url: string;
-      id: number;
-      node_id: string;
-      name: string;
-      label: string;
-      uploader: {
-        login: string;
-        id: number;
-        node_id: string;
-        avatar_url: string;
-        gravatar_id: string;
-        url: string;
-        html_url: string;
-        followers_url: string;
-        following_url: string;
-        gists_url: string;
-        starred_url: string;
-        subscriptions_url: string;
-        organizations_url: string;
-        repos_url: string;
-        events_url: string;
-        received_events_url: string;
-        type: string;
-        site_admin: boolean;
-      };
-      content_type: string;
-      state: string;
-      size: number;
-      download_count: number;
-      created_at: string;
-      updated_at: string;
-      browser_download_url: string;
-    }
-  ];
+  assets: assets[];
   tarball_url: string;
   zipball_url: string;
   body: string;
+}
+
+export interface author {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+export interface assets {
+  url: string;
+  id: number;
+  node_id: string;
+  name: string;
+  label: string;
+  uploader: uploader;
+  content_type: string;
+  state: string;
+  size: number;
+  download_count: number;
+  created_at: string;
+  updated_at: string;
+  browser_download_url: string;
+}
+
+export interface uploader {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
 }

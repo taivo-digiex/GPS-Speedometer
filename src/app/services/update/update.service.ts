919c2e60dc0a23424b6a0974bbf548364ae0ad2f
vdt2210 <vdt1999@outlook.com>
2022-03-26 22:03:59 +0700

    update check network status

--- src/app/services/update/update.service.ts
+++ src/app/services/update/update.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { AlertComponent } from 'src/app/common/components/alert/alert.component';
 import { AppUpdateModel } from 'src/app/common/models/update.model';
 import { App } from '@capacitor/app';
+import { Network } from '@capacitor/network';
 
 @Injectable({
   providedIn: 'root',
@@ -16,44 +17,56 @@ export class UpdateService {
   ) {}
 
   public async checkForUpdate(isManual: boolean) {
-    this.http
-      .get(
-        'https://api.github.com/repos/vdt2210/GPS-Speedometer/releases/latest'
-      )
-      .subscribe(async (info: AppUpdateModel) => {
-        if (info) {
-          const versionNumber = (await App.getInfo()).version;
-          const splittedVersion = versionNumber.split(/[.-]/);
-          const serverVersion = info.tag_name.split(/[.-]/);
+    if ((await Network.getStatus()).connected) {
+      this.http
+        .get(
+          'https://api.github.com/repos/vdt2210/GPS-Speedometer/releases/latest'
+        )
+        .subscribe(async (info: AppUpdateModel) => {
+          if (info) {
+            const versionNumber = (await App.getInfo()).version;
+            const splittedVersion = versionNumber.split(/[.-]/);
+            const serverVersion = info.tag_name.split(/[.-]/);
 
-          if (
-            serverVersion[0] > splittedVersion[0] ||
-            serverVersion[1] > splittedVersion[1] ||
-            serverVersion[2] > splittedVersion[2]
-          ) {
-            this.downloadLatest = info.assets[0].browser_download_url;
-            await this.alertComponent.presentAlert(
-              'ALERT.HEADER.H2',
-              info.tag_name,
-              info.body,
-              null,
-              'COMMON.later',
-              'COMMON.download',
-              (info.assets[0].size * 9.5367431640625e-7).toFixed(2),
-              this,
-              this.downloadNewAppVersion
-            );
-          } else if (isManual) {
-            await this.alertComponent.presentAlertOneBtn(
-              'ALERT.HEADER.H3',
-              versionNumber,
-              'ALERT.MSG.M2',
-              null,
-              'COMMON.ok'
-            );
+            if (
+              serverVersion[0] > splittedVersion[0] ||
+              serverVersion[1] > splittedVersion[1] ||
+              serverVersion[2] > splittedVersion[2]
+            ) {
+              this.downloadLatest = info.assets[0].browser_download_url;
+              await this.alertComponent.presentAlert(
+                'ALERT.HEADER.H2',
+                info.tag_name,
+                info.body,
+                null,
+                'COMMON.later',
+                'COMMON.download',
+                (info.assets[0].size * 9.5367431640625e-7).toFixed(2),
+                this,
+                this.downloadNewAppVersion
+              );
+            } else if (isManual) {
+              await this.alertComponent.presentAlertOneBtn(
+                'ALERT.HEADER.H3',
+                versionNumber,
+                'ALERT.MSG.M2',
+                null,
+                'COMMON.ok'
+              );
+            }
           }
-        }
-      });
+        });
+    } else {
+      if (isManual) {
+        await this.alertComponent.presentAlertOneBtn(
+          'ALERT.HEADER.H4',
+          null,
+          'ALERT.MSG.M3',
+          null,
+          'COMMON.ok'
+        );
+      }
+    }
   }
 
   private async downloadNewAppVersion() {
